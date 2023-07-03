53f066c8e77b738688b2e2e0889d593b53ca8ebe
Ryan <rmschapker@eagles.usi.edu>
2023-07-03 00:50:43 -0500

    Assignment 1
    Game/scene repo creation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    private float moveSpeed;
+    private float jumpForce;
+    private bool isJumping;
+    private float moveHorizontal;
+    private float moveVertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
